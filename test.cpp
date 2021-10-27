b2512727ad6eecc53a03c523d40d69960e7789f9
Marcin <marcin@dot.com>
2021-10-27 15:15:14 +0100

    works

--- test.cpp
+++ test.cpp
@@ -7,38 +7,72 @@ using namespace ::testing;
 
 namespace {
 
+// ##############
+// # simple_pod #
+// ##############
+
 struct simple_pod {
     int x;
     float y;
 };
-using simple = vst::type<simple_pod>;
 
-static_assert(simple{2, 2.f} == simple{2, 2.f});
-static_assert(simple{2, 2.f} != simple{3, 2.f});
-static_assert(simple{2, 2.f} != simple{2, 1.f});
+static constexpr auto get_simple_pod_fields() {
+    return std::tuple{
+        MEMBER(simple_pod, x), 
+        MEMBER(simple_pod, y)};
+}
 
-struct simple_inferred_pod {
+static constexpr auto k_simple_pod_fields = get_simple_pod_fields();
+
+// #############################
+// # simple_self_described_pod #
+// #############################
+
+struct simple_self_described_pod {
     int x;
     float y;
 
-
+    static constexpr auto get_fields() {
+        return std::tuple{
+            MEMBER(simple_self_described_pod, x), 
+            MEMBER(simple_self_described_pod, y)};
+    }
 };
-using simple = vst::type<simple_pod>;
 
-static_assert(simple{2, 2.f} == simple{2, 2.f});
-static_assert(simple{2, 2.f} != simple{3, 2.f});
-static_assert(simple{2, 2.f} != simple{2, 1.f});
+// ########
+// # vsts #
+// ########
 
-}
+using simple                    = vst::type<simple_pod>;
+// using simple_self_described = vst::type<simple_self_described_pod>;
+// using custom_from_func          = vst::type<simple_pod, 
+//                                             vst::with_fields::from_func<get_simple_pod_fields>>;
+// using custom_from_var           = vst::type<simple_pod
+//                                             vst::with_fields::from_func<&k_simple_pod_fields>>;
+
+} // close anon namespace
 
 template <typename T>
-class test_vst : public ::testing::Test {
-};
+class test_vst_equality : public ::testing::Test {};
 
-using MyTypes = ::testing::Types<simple>;
-TYPED_TEST_SUITE(test_vst, MyTypes);
+using equality_types = ::testing::Types<
+    simple
+    // simple_self_described,
+    // simple_from_func,
+    // simple_from_var,
+>;
 
-TYPED_TEST(test_vst, equality)
+TYPED_TEST_SUITE(test_vst_equality, equality_types);
+
+TYPED_TEST(test_vst_equality, test)
 {
-    EXPECT_EQ((TypeParam{2, 2.f}), (TypeParam{2, 3.f}));
+    static_assert(TypeParam{1, 2.f} == TypeParam{1, 2.f});
+    static_assert(TypeParam{2, 2.f} == TypeParam{2, 2.f});
+    static_assert(TypeParam{2, 2.f} != TypeParam{3, 2.f});
+    static_assert(TypeParam{2, 2.f} != TypeParam{2, 1.f});
+
+    EXPECT_EQ((TypeParam{1, 2.f}), (TypeParam{1, 2.f}));
+    EXPECT_EQ((TypeParam{2, 2.f}), (TypeParam{2, 2.f}));
+    EXPECT_NE((TypeParam{2, 2.f}), (TypeParam{3, 2.f}));
+    EXPECT_NE((TypeParam{2, 2.f}), (TypeParam{2, 1.f}));
 }
