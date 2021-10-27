5bc4be2cdce5097f334920a6220b4f626e911a77
Marcin <marcin@dot.com>
2021-10-27 15:49:24 +0100

    works

--- test.cpp
+++ test.cpp
@@ -92,11 +92,14 @@ constexpr bool is_streamable<
 template<typename T, typename ENABLER = void>
 constexpr bool is_hashable = false;
 
+// TODO MG: test for boost hash and boost::hash_value
 template<typename T>
 constexpr bool is_hashable<
     T, 
     std::void_t<
-        decltype(std::declval<const std::hash<T>&>()(std::declval<const T&>()))>>
+        // decltype(std::declval<const std::hash<T>&>())>>
+        // decltype(std::declval<const std::hash<T>&>()(std::declval<const T&>()))>>
+        decltype(boost::hash_value(std::declval<const T&>()))>>
  = true;
 
 } // close anon namespace
@@ -115,15 +118,56 @@ TYPED_TEST_SUITE(test_vst_equality, equality_types);
 
 TYPED_TEST(test_vst_equality, test)
 {
+    static_assert(is_streamable<TypeParam>);
     static_assert(is_equality_comparable<TypeParam>);
     static_assert(!is_comparable<TypeParam>);
+    static_assert(!is_hashable<TypeParam>);
+
     static_assert(TypeParam{1, 2.f} == TypeParam{1, 2.f});
     static_assert(TypeParam{2, 2.f} == TypeParam{2, 2.f});
     static_assert(TypeParam{2, 2.f} != TypeParam{3, 2.f});
     static_assert(TypeParam{2, 2.f} != TypeParam{2, 1.f});
 
-    EXPECT_EQ((TypeParam{1, 2.f}), (TypeParam{1, 2.f}));
-    EXPECT_EQ((TypeParam{2, 2.f}), (TypeParam{2, 2.f}));
-    EXPECT_NE((TypeParam{2, 2.f}), (TypeParam{3, 2.f}));
-    EXPECT_NE((TypeParam{2, 2.f}), (TypeParam{2, 1.f}));
+    EXPECT_TRUE((TypeParam{1, 2.f} == TypeParam{1, 2.f}));
+    EXPECT_TRUE((TypeParam{2, 2.f} == TypeParam{2, 2.f}));
+    EXPECT_TRUE((TypeParam{2, 2.f} != TypeParam{3, 2.f}));
+    EXPECT_TRUE((TypeParam{2, 2.f} != TypeParam{2, 1.f}));
 }
+
+// template <typename T>
+// class test_vst_comparable : public ::testing::Test {};
+
+// using ordered_types = ::testing::Types<
+//     simple_self_described,
+//     custom_from_func,
+//     custom_from_var,
+//     simple
+// >;
+
+// TYPED_TEST_SUITE(test_vst_comparable, equality_types);
+
+// TYPED_TEST(test_vst_comparable, test)
+// {
+//     static_assert(is_streamable<TypeParam>);
+//     static_assert(is_equality_comparable<TypeParam>);
+//     static_assert(is_comparable<TypeParam>);
+//     static_assert(!is_hashable<TypeParam>);
+
+//     static_assert(TypeParam{1, 2.f} <= TypeParam{1, 2.f});
+//     static_assert(TypeParam{1, 2.f} >= TypeParam{1, 2.f});
+//     static_assert(TypeParam{2, 2.f} < TypeParam{3, 2.f});
+//     static_assert(TypeParam{2, 2.f} < TypeParam{2, 3.f});
+//     static_assert(TypeParam{2, 2.f} < TypeParam{3, 3.f});
+//     static_assert(TypeParam{2, 2.f} > TypeParam{2, 1.f});
+//     static_assert(TypeParam{2, 2.f} > TypeParam{1, 2.f});
+//     static_assert(TypeParam{2, 2.f} > TypeParam{1, 1.f});
+    
+//     EXPECT_TRUE(TypeParam{1, 2.f} <= TypeParam{1, 2.f});
+//     EXPECT_TRUE(TypeParam{1, 2.f} >= TypeParam{1, 2.f});
+//     EXPECT_TRUE(TypeParam{2, 2.f} < TypeParam{3, 2.f});
+//     EXPECT_TRUE(TypeParam{2, 2.f} < TypeParam{2, 3.f});
+//     EXPECT_TRUE(TypeParam{2, 2.f} < TypeParam{3, 3.f});
+//     EXPECT_TRUE(TypeParam{2, 2.f} > TypeParam{2, 1.f});
+//     EXPECT_TRUE(TypeParam{2, 2.f} > TypeParam{1, 2.f});
+//     EXPECT_TRUE(TypeParam{2, 2.f} > TypeParam{1, 1.f});
+// }
