6f3d3743f10d020b56fdf0d9e8975cb800b2c15e
Marcin <marcin@dot.com>
2021-11-13 13:07:20 +0000

    works

--- test_named_type.cpp
+++ test_named_type.cpp
@@ -90,11 +90,66 @@ TEST(test_named_type, to_and_from_underlying_no_operators_when_transparent_ops_n
     static_assert( is_comparable<price_transparent, int>);
     static_assert( is_ordered<price_transparent, int>);
 
-    // static_assert(price{4} == 4);
+    constexpr auto eq = std::equal_to<price_transparent>{};
+
     static_assert(price_transparent{4} == 4);
     static_assert(price_transparent{4} != 2);
-    static_assert(std::equal_to<price_transparent>{}(price_transparent{4}, 4));
-    static_assert(!std::equal_to<price_transparent>{}(price_transparent{4}, 2));
+    static_assert(4 == price_transparent{4});
+    static_assert(2 != price_transparent{4});
+
+    EXPECT_TRUE((price_transparent{4} == 4));
+    EXPECT_TRUE((price_transparent{4} != 2));
+    EXPECT_TRUE((4 == price_transparent{4}));
+    EXPECT_TRUE((2 != price_transparent{4}));
+    
+    static_assert( eq(price_transparent{4}, 4));
+    static_assert(!eq(price_transparent{4}, 2));
+    static_assert( eq(4, price_transparent{4}));
+    static_assert(!eq(2, price_transparent{4}));
+
+    constexpr auto lt = std::less<price_transparent>{};
+
+    static_assert(price_transparent{4} < 6);
+    static_assert(price_transparent{4} <= 6);
+    static_assert(price_transparent{4} > 2);
+    static_assert(price_transparent{4} >= 2);
+    static_assert(price_transparent{4} <= 4);
+    static_assert(price_transparent{4} >= 4);
+
+    static_assert(4 < price_transparent{6});
+    static_assert(4 <= price_transparent{6});
+    static_assert(4 > price_transparent{2});
+    static_assert(4 >= price_transparent{2});
+    static_assert(4 <= price_transparent{4});
+    static_assert(4 >= price_transparent{4});
+
+    EXPECT_TRUE((price_transparent{4} < 6));
+    EXPECT_TRUE((price_transparent{4} <= 6));
+    EXPECT_TRUE((price_transparent{4} > 2));
+    EXPECT_TRUE((price_transparent{4} >= 2));
+    EXPECT_TRUE((price_transparent{4} <= 4));
+    EXPECT_TRUE((price_transparent{4} >= 4));
+
+    EXPECT_TRUE((4 < price_transparent{6}));
+    EXPECT_TRUE((4 <= price_transparent{6}));
+    EXPECT_TRUE((4 > price_transparent{2}));
+    EXPECT_TRUE((4 >= price_transparent{2}));
+    EXPECT_TRUE((4 <= price_transparent{4}));
+    EXPECT_TRUE((4 >= price_transparent{4}));
+
+    static_assert( lt(price_transparent{4}, 6));
+    static_assert( lt(price_transparent{4}, 6) || eq(price_transparent{4}, 6));
+    static_assert(!lt(price_transparent{4}, 2) && !eq(price_transparent{4}, 2));
+    static_assert(!lt(price_transparent{4}, 2));
+    static_assert( lt(price_transparent{4}, 4) || eq(price_transparent{4}, 4));
+    static_assert(!lt(price_transparent{4}, 4));
+
+    static_assert( lt(4, price_transparent{6}));
+    static_assert( lt(4, price_transparent{6}) || eq(4, price_transparent{6}));
+    static_assert(!lt(4, price_transparent{2}) && !eq(4, price_transparent{2}));
+    static_assert(!lt(4, price_transparent{2}));
+    static_assert( lt(4, price_transparent{4}) || eq(4, price_transparent{4}));
+    static_assert(!lt(4, price_transparent{4}));
 
     // TODO MG: check for hashable, addable?
 }
