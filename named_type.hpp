8816d9a5dbf04effa42da28e98f65fd68013a696
Marcin <marcin@dot.com>
2021-11-14 11:28:03 +0000

    works

--- named_type.hpp
+++ named_type.hpp
@@ -223,30 +223,6 @@ constexpr bool is_named_type<vst::type<named_type_pod<underlying_t, tag_t, ops_c
 // struct is_named_type<typename named_type_impl<underlying_t, tag_t, type_list<ops...>>::type> : std::true_type {};
 // struct is_named_type<vst::impl::type<named_type<underlying_t, tag_t, ops_category>, type_list<ops...>>> : std::true_type {};
 
-// template<typename underlying_t, typename tag_t, typename props_t>
-// struct named_type_pod
-// {
-//     using props = props_t;
-//     using self = named_type_pod<underlying_t, tag_t, props_t>;
-//     underlying_t value;
-
-//     explicit constexpr named_type_pod(underlying_t value) : value(value) {}
-
-//     explicit constexpr operator const underlying_t&() const { return value; }
-//     explicit constexpr operator underlying_t&() { return value; }
-
-//     constexpr const underlying_t& get() const { return value; }
-//     constexpr underlying_t& get() { return value; }
-
-//     static constexpr auto get_fields()
-//     { 
-//         return std::tuple{&self::value};
-//     }
-// };
-
-// template<typename underlying_t, typename tag_t, typename props_t typename... ops>
-// using named_type = vst::type<named_type_pod<underlying_t, tag_t, props_t>, ops...>;
-
 template<typename T, typename U, std::enable_if_t<is_named_type<T> && T::template is_transparent_with<U>, int> = 0>
 constexpr bool operator==(const T& lhs, const U& rhs)
 {
