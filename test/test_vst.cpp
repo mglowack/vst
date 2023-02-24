cca34aded46b001cc3ca36f17e9540a7f51aadca
Marcin Głowacki <marcin.uk.glowacki@gmail.com>
2023-02-24 14:23:35 +0000

    more

--- test/test_vst.cpp
+++ test/test_vst.cpp
@@ -129,16 +129,21 @@ std::string stringify(const T& o)
 
 } // close anon namespace
 
-TEST(test_vst, apply)
+TEST(test_vst, apply_with_index_return)
 {
-    auto add_indices_and_values = [](const auto... elem) { return (elem.index + ...) + (elem.value + ...); };
+    auto add_values = [](const auto&... elem) { return (elem.value + ...); };
+    auto add_indices_and_values = [](const auto&... elem) { return (elem.index + ...) + (elem.value + ...); };
     
     const std::tuple t_const{3, 4, 5};
     std::tuple t_non_const{3, 4, 5};
 
-    EXPECT_THAT(vst::apply_with_index(add_indices_and_values, std::tuple{3, 4, 5}), Eq(0+3+1+4+2+5));
-    EXPECT_THAT(vst::apply_with_index(add_indices_and_values, t_const),             Eq(0+3+1+4+2+5));
-    EXPECT_THAT(vst::apply_with_index(add_indices_and_values, t_non_const),         Eq(0+3+1+4+2+5));
+    EXPECT_THAT(vst::apply_with_index(add_indices_and_values, std::tuple{3, 4, 5}), Eq(0+3 + 1+4 + 2+5));
+    EXPECT_THAT(vst::apply_with_index(add_indices_and_values, t_const),             Eq(0+3 + 1+4 + 2+5));
+    EXPECT_THAT(vst::apply_with_index(add_indices_and_values, t_non_const),         Eq(0+3 + 1+4 + 2+5));
+
+    EXPECT_THAT(vst::apply_with_index(add_values, std::tuple{3, 4, 5}), Eq(3+4+5));
+    EXPECT_THAT(vst::apply_with_index(add_values, t_const),             Eq(3+4+5));
+    EXPECT_THAT(vst::apply_with_index(add_values, t_non_const),         Eq(3+4+5));
 }
 
 TEST(test_vst, apply_with_index)
