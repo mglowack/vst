d13101435a9eed153661eddd280cdf9d9649b256
Marcin Głowacki <marcin.uk.glowacki@gmail.com>
2023-04-04 17:53:25 +0100

    refactor

--- src/named_type/named_type_trait.h
+++ src/named_type/named_type_trait.h
@@ -6,25 +6,6 @@
 #include "vst.hpp"
 #include "type_list.h"
 
-// ###############################
-// # allows_transparent_ops_with #
-// ###############################
-
-template<typename T>
-struct allows_transparent_ops_with
-{
-    template<typename ops_category_t>
-    struct pred : std::false_type {};
-
-    template<typename U>
-    struct pred<transparent_ops_with<U>> : std::bool_constant<std::is_same_v<T, U>> {};
-};
-
-static_assert( type_list_any_v<type_list<transparent_ops_with<int>>, allows_transparent_ops_with<int>::pred>);
-static_assert( type_list_any_v<type_list<transparent_ops_with<int>, transparent_ops_with<float>>, allows_transparent_ops_with<float>::pred>);
-static_assert(!type_list_any_v<type_list<>, allows_transparent_ops_with<int>::pred>);
-static_assert(!type_list_any_v<type_list<strict_ops>, allows_transparent_ops_with<int>::pred>);
-
 // ####################
 // # named_type_trait #
 // ####################
@@ -54,7 +35,7 @@ struct named_type_trait<named_type_pod<underlying_t, tag_t, op_categories_t, con
 
     template<typename T>
     static constexpr bool is_transparent_with
-        = type_list_any_v<op_categories, allows_transparent_ops_with<T>::template pred>;
+        = type_list_contains_v<op_categories, transparent_ops_with<T>>;
 
     template<typename T>
     static constexpr bool is_implicitly_convertible_to
