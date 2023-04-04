e67462f36a0a6ad2f46e1bfdce383a38ce56ecce
Marcin Głowacki <marcin.uk.glowacki@gmail.com>
2023-04-04 03:27:09 -0700

    builds

--- src/named_type/named_type_pod.h
+++ src/named_type/named_type_pod.h
@@ -1,13 +1,8 @@
 #pragma once
 
-#include <tuple>
+#include <named_type_trait.h>
 
-#include <named_type_conversions_category.h>
-#include <type_list.h>
-template<typename T, typename list>
-constexpr bool is_implicitly_convertible_to = type_list_contains_v<list, implicit_conversions_to<T>>;
-template<typename T, typename list>
-constexpr bool is_implicitly_convertible_from = type_list_contains_v<list, implicit_conversions_from<T>>;
+#include <tuple>
 
 // ##################
 // # named_type_pod #
@@ -16,15 +11,17 @@ constexpr bool is_implicitly_convertible_from = type_list_contains_v<list, impli
 template<typename underlying_t, typename tag_t, typename op_categories_t, typename conversion_categories_t>
 struct named_type_pod
 {
+    using self = named_type_pod<underlying_t, tag_t, op_categories_t, conversion_categories_t>;
+
     underlying_t value;
 
-    explicit(!is_implicitly_convertible_from<underlying_t, conversion_categories_t>)
+    explicit(!named_type_trait<self>::template is_implicitly_convertible_from<underlying_t>)
     constexpr named_type_pod(underlying_t value) : value(value) {}
 
-    explicit(!is_implicitly_convertible_to<underlying_t, conversion_categories_t>)
+    explicit(!named_type_trait<self>::template is_implicitly_convertible_to<underlying_t>)
     constexpr operator const underlying_t&() const { return value; }
 
-    explicit(!is_implicitly_convertible_to<underlying_t, conversion_categories_t>)
+    explicit(!named_type_trait<self>::template is_implicitly_convertible_to<underlying_t>)
     constexpr operator underlying_t&()             { return value; }
 
     constexpr const underlying_t& get()               const { return value; }
@@ -32,6 +29,6 @@ struct named_type_pod
 
     static constexpr auto get_fields()
     {
-        return std::tuple{&named_type_pod<underlying_t, tag_t, op_categories_t, conversion_categories_t>::value};
+        return std::tuple{&self::value};
     }
 };
--- src/named_type/named_type_trait.h
+++ src/named_type/named_type_trait.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <named_type_pod.h>
 #include <named_type_ops_category.h>
+#include <named_type_conversions_category.h>
 
 #include "vst.hpp"
 #include "type_list.h"
@@ -29,11 +29,14 @@ static_assert(!type_list_any_v<type_list<strict_ops>, allows_transparent_ops_wit
 // # named_type_trait #
 // ####################
 
+template<typename underlying_t, typename tag_t, typename op_categories_t, typename conversion_categories_t>
+struct named_type_pod;
+
 template<typename T>
 struct named_type_trait;
 
-template<typename underlying_t, typename tag_t, typename op_categories_t, typename conversion_categories_t, typename... ops>
-struct named_type_trait<vst::type<named_type_pod<underlying_t, tag_t, op_categories_t, conversion_categories_t>, ops...>>
+template<typename underlying_t, typename tag_t, typename op_categories_t, typename conversion_categories_t>
+struct named_type_trait<named_type_pod<underlying_t, tag_t, op_categories_t, conversion_categories_t>>
 {
     static constexpr bool exists = true;
 
@@ -46,7 +49,19 @@ struct named_type_trait<vst::type<named_type_pod<underlying_t, tag_t, op_categor
         = type_list_any_v<op_categories, allows_transparent_ops_with<T>::template pred>;
 
     static constexpr bool is_transparent = is_transparent_with<underlying_type>;
+
+    template<typename T>
+    static constexpr bool is_implicitly_convertible_to
+        = type_list_contains_v<conversion_categories, implicit_conversions_to<T>>;
+
+    template<typename T>
+    static constexpr bool is_implicitly_convertible_from
+        = type_list_contains_v<conversion_categories, implicit_conversions_from<T>>;
 };
+
+template<typename underlying_t, typename tag_t, typename op_categories_t, typename conversion_categories_t, typename... ops>
+struct named_type_trait<vst::type<named_type_pod<underlying_t, tag_t, op_categories_t, conversion_categories_t>, ops...>>
+: named_type_trait<named_type_pod<underlying_t, tag_t, op_categories_t, conversion_categories_t>> {};
 
 template<typename T>
 concept NamedType = named_type_trait<T>::exists;
