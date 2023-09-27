a945e7982120f17858956fdc0f34cfb272517091
Marcin Głowacki <marcin.uk.glowacki@gmail.com>
2023-09-27 14:41:15 +0100

    vst::trait add more contraints

--- src/vst/vst_trait.h
+++ src/vst/vst_trait.h
@@ -28,7 +28,7 @@ struct trait
 } // namespace impl
 
 // no 'with_fields::X' specified => injecting 'with_fields::use_default'
-template<typename T, typename... ops> // why not Operator... ?
+template<typename T, Operator... ops>
 struct trait<type<T, ops...>>
 : trait<type<T, with_fields::use_default, ops...>>
 {
@@ -37,13 +37,28 @@ struct trait<type<T, ops...>>
 // 'with_fields::use_default':
 // * translate to 'with_fields::from<T>' if 'get_fields()' is provided on the underlying type
 // * translate to 'with_fields::from_aggregate' otherwise
-template<SelfDescribed T, typename... ops> // why not Operator... ?
+#if 1
+// NOTE: workaround for https://stackoverflow.com/questions/77187649/c20-partial-class-specialization-with-concepts-and-variadic-template-args
+template<SelfDescribed T, Operator o1, Operator... ops>
+struct trait<type<T, with_fields::use_default, o1, ops...>>
+: trait<type<T, with_fields::from<T>, o1, ops...>>
+{
+};
+
+template<SelfDescribed T>
+struct trait<type<T, with_fields::use_default>>
+: trait<type<T, with_fields::from<T>>>
+{
+};
+#else
+template<SelfDescribed T, Operator... ops>
 struct trait<type<T, with_fields::use_default, ops...>>
 : trait<type<T, with_fields::from<T>, ops...>>
 {
 };
+#endif
 
-template<typename T, typename... ops> // why not Operator... ?
+template<typename T, Operator... ops>
 struct trait<type<T, with_fields::use_default, ops...>>
 : trait<type<T, with_fields::from_aggregate, ops...>>
 {
